747e2c9f4cf4224cc09c4ca28ae4496163c075b5
nich23 <hyndrom@gmail.com>
2016-12-20 17:35:29 +0200

    1

--- Developer/src/demo/tool/Test.java
+++ Developer/src/demo/tool/Test.java
@@ -1,36 +1,15 @@
 package demo.tool;
 
-class GS{
-	private GS[] myDog = new GS[10];
-	//
-	
-	public void setMyDog(int y) {
-		myDog[7] = new GS();
-		myDog[7] = y;
-	}
-	public int getMyDog(){
-		return myDog[7];
-	}
-	//
-	private int[] tester = new int[5];
-	public void setTester(int x) {
-		tester[3] = x;
-	}
-	public int getTester() {
-		return tester[3];
-	}
-	//
-}
-
 public class Test {
 
 	public static void main(String[] args) {
-		GS one = new GS();
-		GS two = new GS();
-		two.setMyDog(66);
-		one.setTester(28);
-		System.out.println(two.getMyDog());
-		System.out.println(one.getTester());
+		for (int x = 0; x < 1; x++) {
+			if(x == 0) {
+				System.out.println(x);
+				x++;
+			}
+			System.out.println(x);
+		}
 	}
 
 }
