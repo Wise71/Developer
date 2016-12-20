63348e4362ffce8da164968722f322fe2d5ca83e
nich23 <hyndrom@gmail.com>
2016-12-20 14:20:21 +0200

    Change

--- Developer/src/demo/tool/Books.java
+++ Developer/src/demo/tool/Books.java
@@ -23,6 +23,7 @@ class BooksTestDrive {
 			System.out.print(myBooks[x].title);
 			System.out.print(" by ");
 			System.out.println(myBooks[x].author);
+			System.out.println("Testing..");
 			x = x + 1;
 		}
 	}
