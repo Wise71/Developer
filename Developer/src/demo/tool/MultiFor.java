4c18e2427e68f66e6052195d563a0cd6bdada850
nich23 <hyndrom@gmail.com>
2016-12-20 15:14:39 +0200

    1

---
+++ Developer/src/demo/tool/MultiFor.java
@@ -0,0 +1,10 @@
+package demo.tool;
+
+public class MultiFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Developer/src/demo/tool/Test.java
+++ Developer/src/demo/tool/Test.java
@@ -1,10 +1,36 @@
 package demo.tool;
 
+class GS{
+	private GS[] myDog = new GS[10];
+	//
+	
+	public void setMyDog(int y) {
+		myDog[7] = new GS();
+		myDog[7] = y;
+	}
+	public int getMyDog(){
+		return myDog[7];
+	}
+	//
+	private int[] tester = new int[5];
+	public void setTester(int x) {
+		tester[3] = x;
+	}
+	public int getTester() {
+		return tester[3];
+	}
+	//
+}
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		GS one = new GS();
+		GS two = new GS();
+		two.setMyDog(66);
+		one.setTester(28);
+		System.out.println(two.getMyDog());
+		System.out.println(one.getTester());
 	}
 
 }
