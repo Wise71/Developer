69ba2296378f3506a441d904c3c9c28c5fb68be6
nich23 <hyndrom@gmail.com>
2017-01-05 18:22:11 +0200

    Chat server

--- Developer/src/demo/tool/Test.java
+++ Developer/src/demo/tool/Test.java
@@ -1,9 +1,5 @@
 package demo.tool;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
  
 public class Test {
  
