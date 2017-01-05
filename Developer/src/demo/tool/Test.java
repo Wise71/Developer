31280c5f391f879f06085a57cd453eac83359e19
nich23 <hyndrom@gmail.com>
2017-01-05 18:17:58 +0200

    Test

--- Developer/src/demo/tool/Test.java
+++ Developer/src/demo/tool/Test.java
@@ -1,22 +1,23 @@
-package demo.tool;
-
 import java.io.*;
  
 public class Test {
  
     public static void main(String[] args) {
-    	try(BufferedReader br = new BufferedReader(new FileReader("C:\\notes5.txt")))
-    	{
-    	    //чтение построчно
-    	    String s;
-    	    while((s=br.readLine())!=null){
-    	                 
-    	        System.out.println(s);
-    	    }
-    	}
-    	 catch(IOException ex){
-    	             
-    	    System.out.println(ex.getMessage() +"aaaa");
-    	} 
+         
+        try(BufferedReader br = new BufferedReader (new InputStreamReader(System.in)); 
+                BufferedWriter bw = new BufferedWriter(new FileWriter("D:\\notes5.txt")))
+        {
+           // чтение построчно
+            String text;
+            while(!(text=br.readLine()).equals("ESC")){
+                  
+                bw.write(text + "\n");
+                bw.flush();
+            }
+        }
+        catch(IOException ex){
+              
+            System.out.println(ex.getMessage());
+        } 
     }   
 }
